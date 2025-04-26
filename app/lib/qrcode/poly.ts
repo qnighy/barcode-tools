0455f7961d627aeb920371f2e7010dbd470f36cd
Masaki Hara <ackie.h.gmai@gmail.com>
2025-04-26 16:04:39 +0900

    Add polynomial

---
+++ app/lib/qrcode/poly.ts
@@ -0,0 +1,50 @@
+import { add, addExponents, exp, GF256, log, LogGF256, ONE } from "./gf256";
+
+/**
+ * A divisor polynomial for GF(256)-based Reed-Solomon coding.
+ */
+export class RSPolynomial {
+  readonly nonLeadingLogCoefficients: readonly LogGF256[];
+
+  constructor(coefficients: readonly GF256[]) {
+    if (coefficients.length === 0) {
+      throw new Error("Coefficients cannot be empty");
+    }
+    if (coefficients[coefficients.length - 1] !== 1) {
+      throw new Error("Leading coefficient must be 1");
+    }
+    const nonLeadingLogCoefficients = Object.freeze<readonly LogGF256[]>(
+      coefficients.slice(0, coefficients.length - 1).map((c) => log(c))
+    );
+    this.nonLeadingLogCoefficients = nonLeadingLogCoefficients;
+    Object.freeze(this);
+  }
+}
+
+const MAX_DEGREE = 68;
+const ALPHA = 2 as GF256;
+function generatePolynomials(): readonly RSPolynomial[] {
+  const coefficients: GF256[] = [ONE];
+  const polynomials: RSPolynomial[] = [];
+  const logAlpha = log(ALPHA);
+  let logPowerOfAlpha = log(ONE);
+  while (polynomials.length <= MAX_DEGREE) {
+    polynomials.push(new RSPolynomial(coefficients));
+
+    const k = coefficients.length;
+    coefficients.push(ONE);
+    for (let i = k - 1; i >= 0; i--) {
+      if (i > 0) {
+        coefficients[i] = add(coefficients[i - 1], exp(addExponents(logPowerOfAlpha, log(coefficients[i]))));
+      } else {
+        coefficients[i] = exp(addExponents(logPowerOfAlpha, log(coefficients[i])));
+      }
+    }
+
+    logPowerOfAlpha = addExponents(logPowerOfAlpha, logAlpha);
+  }
+  Object.freeze(polynomials);
+  return polynomials;
+}
+
+export const POLYNOMIALS: readonly RSPolynomial[] = generatePolynomials();

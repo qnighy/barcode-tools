7f4ecfd3140920cbcce37ed7e3afab7688c714e1
Masaki Hara <ackie.h.gmai@gmail.com>
2025-04-29 19:01:35 +0900

    Add BCH6 encoder

--- app/lib/qrcode/bch.ts
+++ app/lib/qrcode/bch.ts
@@ -19,3 +19,26 @@ function generateBCH5Table(): number[] {
   return Array.from({ length: 1 << 5 }, (_, i) => naiveEncodeBCH5(i));
 }
 const BCH5_TABLE = generateBCH5Table();
+
+// x^12 + x^11 + x^10 + x^9 + x^8 + x^5 + x^2 + 1
+const BCH6_DIVISOR = 0b1111100100101;
+
+export function encodeBCH6(value: number): number {
+  return BCH6_TABLE[value - BCH6_TABLE_OFFSET];
+}
+
+function naiveEncodeBCH6(value: number): number {
+  let rem = value << 12;
+  for (let i = 5; i >= 0; i--) {
+    if (rem & (1 << (i + 12))) {
+      rem ^= BCH6_DIVISOR << i;
+    }
+  }
+  return (value << 12) | rem;
+}
+
+function generateBCH6Table(): number[] {
+  return Array.from({ length: 40 - BCH6_TABLE_OFFSET + 1 }, (_, i) => naiveEncodeBCH6(i + BCH6_TABLE_OFFSET));
+}
+const BCH6_TABLE_OFFSET = 7;
+const BCH6_TABLE = generateBCH6Table();

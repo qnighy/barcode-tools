8e9193728a49dd9d15d3d008ac503448d5231ad9
Masaki Hara <ackie.h.gmai@gmail.com>
2025-05-04 08:54:27 +0900

    Rewrite bitPositions

--- app/lib/qrcode/layout.ts
+++ app/lib/qrcode/layout.ts
@@ -1,4 +1,4 @@
-import { isMicroQRVersion, MicroQRVersion, QRVersion, SPECS, Version } from "./specs";
+import { isMicroQRVersion, SPECS, Version } from "./specs";
 
 export const FUNCTION_PATTERN = 2;
 export const METADATA_AREA = 4;
@@ -129,126 +129,36 @@ function alignmentParity(x: number, y: number): number {
   return parity & 1;
 }
 
-export function bitPositions(version: Version): IterableIterator<[number, number]> {
-  if (isMicroQRVersion(version)) {
-    return microQrBitPositions(version);
-  } else {
-    return qrBitPositions(version);
+export function* bitPositions(version: Version): IterableIterator<[number, number]> {
+  const { width } = SPECS[version];
+  const height = width;
+  const mat = new Uint8Array(width * height);
+  fillFunctionPatterns(mat, version);
+  for (const [x, y] of rawBitPositions(version)) {
+    if (mat[y * width + x] & -2) {
+      continue;
+    }
+    yield [x, y];
   }
 }
 
-function* microQrBitPositions(version: MicroQRVersion): IterableIterator<[number, number]> {
-  const versionNumber = Number(version.slice(1));
-  for (let x = 7 + versionNumber * 2; x >= 1; x -= 2) {
-    const minY = x >= 9 ? 1 : 9;
-    const isDescending = (x + versionNumber * 2) % 4 === 1;
+function* rawBitPositions(version: Version): IterableIterator<[number, number]> {
+  const { width } = SPECS[version];
+  const height = width;
+  const timingPatternX = isMicroQRVersion(version) ? 0 : 6;
+  const numColumns = (width - 1) / 2;
+  for (let col = numColumns - 1; col >= 0; col--) {
+    const x = col * 2 + Number(col >= timingPatternX / 2);
+    const isDescending = (numColumns - 1 - col) % 2 > 0;
     if (isDescending) {
-      for (let y = minY; y < 9 + versionNumber * 2; y++) {
+      for (let y = 0; y < height; y++) {
         yield [x + 1, y];
         yield [x, y];
       }
     } else {
-      for (let y = 9 + versionNumber * 2 - 1; y >= minY; y--) {
+      for (let y = height - 1; y >= 0; y--) {
         yield [x + 1, y];
         yield [x, y];
-      }
-    }
-  }
-}
-
-function* qrBitPositions(version: QRVersion): IterableIterator<[number, number]> {
-  const { width, versionInfoSize, alignmentPatternPositions } = SPECS[version];
-  const numFatColumns = version * 2 + 8;
-  let currentXAlignmentIndex = alignmentPatternPositions.length - 1;
-  for (let fatCol = numFatColumns - 1; fatCol >= 0; fatCol--) {
-    // The column consists of x = xBase line and x = xBase + 1 line.
-    // Here the computation skips x = 6 (which contains the vertical timing pattern)
-    const xBase = fatCol * 2 + Number(fatCol >= 3);
-    // Ignore upper finder patterns, separators,
-    // format information, and a part of version information.
-    const minY =
-      fatCol < 4 || fatCol >= numFatColumns - 4
-      ? 9
-      : fatCol >= numFatColumns - 5 && versionInfoSize
-      ? 7
-      : 0;
-    // Ignore lower-left finder pattern, separators, and version information.
-    const maxY =
-      fatCol < 3 && versionInfoSize
-      ? width - 11
-      : fatCol < 4
-      ? width - 8
-      : width;
-
-    if (currentXAlignmentIndex > 0 && xBase + 4 <= alignmentPatternPositions[currentXAlignmentIndex]) {
-      --currentXAlignmentIndex;
-    }
-    const alignmentXStart = currentXAlignmentIndex >= 0 ? alignmentPatternPositions[currentXAlignmentIndex] - 2 : -5;
-    const alignmentXEnd = alignmentXStart + 5;
-    const hasAlignmentX0 = xBase >= alignmentXStart && xBase < alignmentXEnd;
-    const hasAlignmentX1 = xBase + 1 >= alignmentXStart && xBase + 1 < alignmentXEnd;
-    const hasAlignmentX = hasAlignmentX0 || hasAlignmentX1;
-    // Ignore top-right and top-left alignment patterns
-    const minYAlignmentIndex =
-      currentXAlignmentIndex === 0 || currentXAlignmentIndex === alignmentPatternPositions.length - 1
-      ? 1
-      : 0;
-    // Ignore bottom-right alignment patterns
-    const maxYAlignmentIndex =
-      currentXAlignmentIndex === 0
-      ? alignmentPatternPositions.length - 1
-      : alignmentPatternPositions.length;
-    // The remaining version info part that is not covered by minY/maxY definition.
-    // It always suppresses baseX + 1 position and not baseX + 0 position.
-    const hasVersionInfo = !!versionInfoSize && fatCol === numFatColumns - 6;
-    const descending = fatCol % 2 === 0;
-    if (descending) {
-      let currentYAlignmentIndex = minYAlignmentIndex;
-      for (let y = minY; y < maxY; y++) {
-        if (y === 6) {
-          // Skip the horizontal timing pattern.
-          continue;
-        }
-        if (
-          hasAlignmentX &&
-          currentYAlignmentIndex + 1 < maxYAlignmentIndex &&
-          y >= alignmentPatternPositions[currentYAlignmentIndex] + 3
-        ) {
-          currentYAlignmentIndex++;
-        }
-        const alignmentYStart = currentYAlignmentIndex < maxYAlignmentIndex ? alignmentPatternPositions[currentYAlignmentIndex] - 2 : -5;
-        const alignmentYEnd = alignmentYStart + 5;
-        const hasAlignmentY = y >= alignmentYStart && y < alignmentYEnd;
-        if (!(hasAlignmentY && hasAlignmentX1) && !(hasVersionInfo && y < 6)) {
-          yield [xBase + 1, y];
-        }
-        if (!(hasAlignmentY && hasAlignmentX0)) {
-          yield [xBase, y];
-        }
-      }
-    } else {
-      let currentYAlignmentIndex = maxYAlignmentIndex - 1;
-      for (let y = maxY - 1; y >= minY; y--) {
-        if (y === 6) {
-          // Skip the horizontal timing pattern.
-          continue;
-        }
-        if (
-          hasAlignmentX &&
-          currentYAlignmentIndex > minYAlignmentIndex &&
-          y + 4 < alignmentPatternPositions[currentYAlignmentIndex]
-        ) {
-          currentYAlignmentIndex--;
-        }
-        const alignmentYStart = currentYAlignmentIndex >= minYAlignmentIndex ? alignmentPatternPositions[currentYAlignmentIndex] - 2 : -5;
-        const alignmentYEnd = alignmentYStart + 5;
-        const hasAlignmentY = y >= alignmentYStart && y < alignmentYEnd;
-        if (!(hasAlignmentY && hasAlignmentX1) && !(hasVersionInfo && y < 6)) {
-          yield [xBase + 1, y];
-        }
-        if (!(hasAlignmentY && hasAlignmentX0)) {
-          yield [xBase, y];
-        }
       }
     }
   }

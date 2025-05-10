5f9a1a1c54197557150fb3a425ef7f53cb9dadba
Masaki Hara <ackie.h.gmai@gmail.com>
2025-05-10 15:33:45 +0900

    Rework thresholding

--- app/lib/qrcode/thresholding.test.ts
+++ app/lib/qrcode/thresholding.test.ts
@@ -4,7 +4,7 @@ import { test } from "vitest";
 import { PNG } from "pngjs";
 import { threshold } from "./thresholding";
 
-test("image thresholding", async () => {
+test("image thresholding (gaussianSigma = 32)", async () => {
   const data = await fs.promises.readFile(
     path.join(__dirname, "./fixtures/qr-test1-small.png")
   );
@@ -25,12 +25,49 @@ test("image thresholding", async () => {
       png.data.buffer as ArrayBuffer,
       png.data.byteOffset,
       png.data.length
-    )
+    ),
+    32
   );
 
   await new Promise<void>((resolve, reject) => {
     png.pack().pipe(
-      fs.createWriteStream(path.join(__dirname, "./fixtures/qr-test1-small-thresholded.png"))
+      fs.createWriteStream(path.join(__dirname, "./fixtures/qr-test1-small-thresholded-32.png"))
+    ).on("finish", () => {
+      resolve();
+    }).on("error", (error) => {
+      reject(error);
+    });
+  });
+});
+
+test("image thresholding (gaussianSigma = 256)", async () => {
+  const data = await fs.promises.readFile(
+    path.join(__dirname, "./fixtures/qr-test1-small.png")
+  );
+  const png = await new Promise<PNG>((resolve, reject) => {
+    new PNG({ filterType: 4 }).parse(data, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+  console.log(png.gamma);
+  threshold(
+    png.width,
+    png.height,
+    new Uint8ClampedArray(
+      png.data.buffer as ArrayBuffer,
+      png.data.byteOffset,
+      png.data.length
+    ),
+    256
+  );
+
+  await new Promise<void>((resolve, reject) => {
+    png.pack().pipe(
+      fs.createWriteStream(path.join(__dirname, "./fixtures/qr-test1-small-thresholded-256.png"))
     ).on("finish", () => {
       resolve();
     }).on("error", (error) => {
--- app/lib/qrcode/thresholding.ts
+++ app/lib/qrcode/thresholding.ts
@@ -2,30 +2,171 @@ export function threshold(
   width: number,
   height: number,
   data: Uint8ClampedArray<ArrayBuffer>,
+  gaussianSigma: number
 ): void {
-  let luminanceSum = 0;
+  const luminances = new Matrix<number, Float32Array>(width, height, new Float32Array(width * height));
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
       const r = toLinear(data[(y * width + x) * 4] / 255);
       const g = toLinear(data[(y * width + x) * 4 + 1] / 255);
       const b = toLinear(data[(y * width + x) * 4 + 2] / 255);
       const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-      luminanceSum += luminance;
+      luminances.setAt(x, y, luminance);
     }
   }
-  const luminanceAvg = luminanceSum / (width * height);
+  const afterGaussian = applyGaussian(luminances, gaussianSigma);
+
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      const r = toLinear(data[(y * width + x) * 4] / 255);
-      const g = toLinear(data[(y * width + x) * 4 + 1] / 255);
-      const b = toLinear(data[(y * width + x) * 4 + 2] / 255);
-      const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-      const newValue = luminance > luminanceAvg ? 1 : 0;
-      data[(y * width + x) * 4] = newValue * 255;
-      data[(y * width + x) * 4 + 1] = newValue * 255;
-      data[(y * width + x) * 4 + 2] = newValue * 255;
+      const newValue = Number(luminances.getAt(x, y) > afterGaussian.getAt(x, y));
+      data[(y * width + x) * 4 + 0] = fromLinear(newValue) * 255;
+      data[(y * width + x) * 4 + 1] = fromLinear(newValue) * 255;
+      data[(y * width + x) * 4 + 2] = fromLinear(newValue) * 255;
     }
   }
+  return;
+}
+
+class Matrix<T, A extends ArrayLike<T>> {
+  array: A;
+  width: number;
+  height: number;
+  constructor(width: number, height: number, array: A) {
+    if (array.length !== width * height) {
+      throw new RangeError('Array length does not match width and height');
+    }
+    this.array = array;
+    this.width = width;
+    this.height = height;
+  }
+
+  getAt(x: number, y: number): T {
+    if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+      return undefined as unknown as T;
+    }
+    return this.array[y * this.width + x];
+  }
+  setAt(x: number, y: number, value: T): void {
+    if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+      throw new RangeError('Index out of bounds');
+    }
+    (this.array as unknown as T[])[y * this.width + x] = value;
+  }
+}
+
+function applyGaussian(
+  input: Matrix<number, Float32Array>,
+  gaussianSigma: number,
+): Matrix<number, Float32Array> {
+  if (gaussianSigma < 40) {
+    return applyGaussianA(input, gaussianSigma);
+  } else {
+    return applyGaussianB(input, gaussianSigma);
+  }
+}
+
+function applyGaussianA(
+  input: Matrix<number, Float32Array>,
+  gaussianSigma: number,
+): Matrix<number, Float32Array> {
+  const gaussianRadius = Math.ceil(gaussianSigma * 1.5);
+  const gaussianCoefficients = new Float32Array(gaussianRadius * 2 + 1);
+  let gaussianSum = 0;
+  for (let i = -gaussianRadius; i <= gaussianRadius; i++) {
+    const coefficient = Math.exp(-((i / gaussianSigma) ** 2));
+    gaussianCoefficients[i + gaussianRadius] = coefficient;
+    gaussianSum += coefficient;
+  }
+  for (let i = 0; i < gaussianCoefficients.length; i++) {
+    gaussianCoefficients[i] /= gaussianSum;
+  }
+
+  const gaussX = new Matrix<number, Float32Array>(input.width, input.height, new Float32Array(input.width * input.height));
+  for (let y = 0; y < input.height; y++) {
+    for (let x = 0; x < input.width; x++) {
+      let sum = 0;
+      for (let i = -gaussianRadius; i <= gaussianRadius; i++) {
+        const x2 = Math.max(0, Math.min(x + i, input.width - 1));
+        sum += input.getAt(x2, y) * gaussianCoefficients[i + gaussianRadius];
+      }
+      gaussX.setAt(x, y, sum);
+    }
+  }
+  const output = new Matrix<number, Float32Array>(input.width, input.height, new Float32Array(input.width * input.height));
+  for (let y = 0; y < input.height; y++) {
+    for (let x = 0; x < input.width; x++) {
+      let sum = 0;
+      for (let i = -gaussianRadius; i <= gaussianRadius; i++) {
+        const y2 = Math.max(0, Math.min(y + i, input.height - 1));
+        sum += gaussX.getAt(x, y2) * gaussianCoefficients[i + gaussianRadius];
+      }
+      output.setAt(x, y, sum);
+    }
+  }
+  return output;
+}
+
+function applyGaussianB(
+  input: Matrix<number, Float32Array>,
+  gaussianSigma: number,
+): Matrix<number, Float32Array> {
+  const { width, height } = input;
+  const NUM_PASS = 3;
+  const radius = Math.ceil(Math.sqrt(3 * gaussianSigma ** 2 / NUM_PASS));
+  const output = new Matrix<number, Float32Array>(width, height, new Float32Array(width * height));
+  output.array.set(input.array);
+  const tmp = new Matrix<number, Float32Array>(width, height, new Float32Array(width * height));
+  for (let i = 0; i < NUM_PASS; i++) {
+    const input = output;
+
+    // Fold in the x direction
+    for (let y = 0; y < height; y++) {
+      let sum = 0;
+      let windowWidth = 0;
+      for (let x = -radius; x < 0; x++) {
+        if (x + radius < width) {
+          sum += input.getAt(x + radius, y);
+          windowWidth++;
+        }
+      }
+      for (let x = 0; x < width; x++) {
+        if (x + radius < width) {
+          sum += input.getAt(x + radius, y);
+          windowWidth++;
+        }
+        tmp.setAt(x, y, sum / windowWidth);
+        if (x - radius >= 0) {
+          sum -= input.getAt(x - radius, y);
+          windowWidth--;
+        }
+      }
+    }
+
+    // Fold in the y direction
+    for (let x = 0; x < width; x++) {
+      let sum = 0;
+      let windowWidth = 0;
+      for (let y = -radius; y < 0; y++) {
+        if (y + radius < height) {
+          sum += tmp.getAt(x, y + radius);
+          windowWidth++;
+        }
+      }
+      for (let y = 0; y < height; y++) {
+        if (y + radius < height) {
+          sum += tmp.getAt(x, y + radius);
+          windowWidth++;
+        }
+        output.setAt(x, y, sum / windowWidth);
+        if (y - radius >= 0) {
+          sum -= tmp.getAt(x, y - radius);
+          windowWidth--;
+        }
+      }
+    }
+  }
+
+  return output;
 }
 
 function toLinear(value: number): number {
@@ -33,5 +174,13 @@ function toLinear(value: number): number {
     return value / 12.92;
   } else {
     return ((value + 0.055) / 1.055) ** 2.4;
+  }
+}
+
+function fromLinear(value: number): number {
+  if (value <= 0.0031308) {
+    return value * 12.92;
+  } else {
+    return 1.055 * value ** (1 / 2.4) - 0.055;
   }
 }
